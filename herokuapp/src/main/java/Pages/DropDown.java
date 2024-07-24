31cbf00a11a06172a5ddf3a40475c818b8f96330
kayatk <thillaikayatk@gmail.com>
2024-07-24 22:30:37 +0200

    Dropdown script update

--- herokuapp/src/main/java/Pages/DropDown.java
+++ herokuapp/src/main/java/Pages/DropDown.java
@@ -1,10 +1,13 @@
 package Pages;
 
+import static org.junit.Assert.assertThat;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class DropDown {
     public static void main(String args[]){
@@ -14,6 +17,9 @@ public class DropDown {
        driver.get("https://the-internet.herokuapp.com/dropdown");
        WebElement title =driver.findElement(By.cssSelector("h3"));
        Assert.assertTrue(title.getText().contains("Dropdown List"));
+       Select select =new Select(driver.findElement(By.id("dropdown")));
+       select.selectByVisibleText("Option 1");
+      Assert.assertEquals(select.getFirstSelectedOption(),"Option 1");
 
     }
     
