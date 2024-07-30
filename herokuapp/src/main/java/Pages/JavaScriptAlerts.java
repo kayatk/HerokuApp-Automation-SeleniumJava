91d4f7f6f529b5cc026488d9625531db0cadbcc9
kayatk <thillaikayatk@gmail.com>
2024-07-30 12:48:38 +0200

    javascript alet script

--- herokuapp/src/main/java/Pages/JavaScriptAlerts.java
+++ herokuapp/src/main/java/Pages/JavaScriptAlerts.java
@@ -1,6 +1,8 @@
 package Pages;
 
 import org.junit.Assert;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,9 +11,31 @@ public class JavaScriptAlerts {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/javascript_alerts");
-        Assert.assertTrue(driver.getCurrentUrl().concat("javascript_alerts"));
+        Assert.assertTrue(driver.getCurrentUrl().contains("javascript_alerts"));
+
+        //Js Alert
+        driver.findElement(By.xpath("//button[contains(text(),'Alert')]")).click();
+        Alert alert = driver.switchTo().alert();
+        Assert.assertEquals(alert.getText(), "I am a JS Alert");
+        alert.accept();
+        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='You successfully clicked an alert']")).isDisplayed());
 
 
+        //JS Confirm
+        driver.findElement(By.xpath("//button[contains(text(),'Confirm')]")).click();
+        Alert confirm = driver.switchTo().alert();
+        Assert.assertEquals(confirm.getText(),"I am a JS Confirm");
+        confirm.dismiss();
+        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='You successfully clicked an alert']")).isDisplayed());
+        driver.findElement(By.xpath("//button[contains(text(),'Confirm')]")).click();
+        confirm.accept();
+        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='You successfully clicked an alert']")).isDisplayed());
+
+
+        //JS Prompt
+        driver.findElement(By.xpath("//button[contains(text(),'Prompt')]")).click();
+        Alert prompt = driver.switchTo().alert();
+        Assert.assertEquals(confirm.getText(),"II am a JS prompt");
 
     }
     
