2343c001f075da1c9fd04f9ebd8d463bd61b79cd
kayatk <thillaikayatk@gmail.com>
2024-07-30 12:56:28 +0200

    javascript alert script

--- herokuapp/src/main/java/Pages/JavaScriptAlerts.java
+++ herokuapp/src/main/java/Pages/JavaScriptAlerts.java
@@ -15,27 +15,30 @@ public class JavaScriptAlerts {
 
         //Js Alert
         driver.findElement(By.xpath("//button[contains(text(),'Alert')]")).click();
-        Alert alert = driver.switchTo().alert();
-        Assert.assertEquals(alert.getText(), "I am a JS Alert");
-        alert.accept();
+        Assert.assertEquals(driver.switchTo().alert().getText(), "I am a JS Alert");
+        driver.switchTo().alert().accept();
         Assert.assertTrue(driver.findElement(By.xpath("//p[text()='You successfully clicked an alert']")).isDisplayed());
 
 
         //JS Confirm
         driver.findElement(By.xpath("//button[contains(text(),'Confirm')]")).click();
-        Alert confirm = driver.switchTo().alert();
-        Assert.assertEquals(confirm.getText(),"I am a JS Confirm");
-        confirm.dismiss();
-        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='You successfully clicked an alert']")).isDisplayed());
+        Assert.assertEquals(driver.switchTo().alert().getText(),"I am a JS Confirm");
+        driver.switchTo().alert().dismiss();
+        Assert.assertTrue(driver.findElement(By.xpath("You clicked: Cancel")).isDisplayed());
         driver.findElement(By.xpath("//button[contains(text(),'Confirm')]")).click();
-        confirm.accept();
-        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='You successfully clicked an alert']")).isDisplayed());
+        driver.switchTo().alert().accept();
+        Assert.assertTrue(driver.findElement(By.xpath("You clicked: Ok")).isDisplayed());
 
 
         //JS Prompt
         driver.findElement(By.xpath("//button[contains(text(),'Prompt')]")).click();
-        Alert prompt = driver.switchTo().alert();
-        Assert.assertEquals(confirm.getText(),"II am a JS prompt");
+        String msg ="Text";
+        driver.switchTo().alert().sendKeys(msg);
+        driver.switchTo().alert().accept();
+        Assert.assertEquals(driver.switchTo().alert().getText(),"You entered: "+msg);
+        driver.findElement(By.xpath("//button[contains(text(),'Prompt')]")).click();
+        driver.switchTo().alert().dismiss();
+        
 
     }
     
