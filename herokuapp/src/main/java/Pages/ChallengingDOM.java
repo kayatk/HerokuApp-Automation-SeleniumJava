dd64fcc8dc503244284f7d6fb4f3b38dd79d7848
kayatk <thillaikayatk@gmail.com>
2024-07-31 18:19:39 +0200

    Screenshot with highlighted element

--- herokuapp/src/main/java/Pages/ChallengingDOM.java
+++ herokuapp/src/main/java/Pages/ChallengingDOM.java
@@ -1,8 +1,16 @@
 package Pages;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.locators.RelativeLocator;
 
@@ -14,7 +22,21 @@ public class ChallengingDOM {
         Assert.assertTrue(driver.getCurrentUrl().contains("challenging_dom"));
 
         //Locate the element using Relative elements/Friendly Locator
-       driver.findElement(RelativeLocator.with(By.tagName("td")).below(By.xpath("//tbody//td[1]")).above(By.xpath("//tbody//tr[3]")));
+       // driver.findElement(RelativeLocator.with(By.tagName("td")).below(By.xpath("//tbody//td[1]")).above(By.xpath("//tbody//tr[3]")));
+       
+       
+       WebElement ele = driver.findElement(By.xpath("//tbody//td[1]"));
+       JavascriptExecutor js = (JavascriptExecutor)driver;
+       js.executeScript("arguments[0].style.border='3px Solid red'",ele);
+       TakesScreenshot ts = (TakesScreenshot)driver;
+       File src=ts.getScreenshotAs(OutputType.FILE);
+       File f = new File("herokuapp/src/main/resources/Highlighted.png");
+        try {
+            FileUtils.copyFile(src,f);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         driver.quit();
     }
     
