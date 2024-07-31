ad5c7f22667a170bfbb90ca9ed9f3c1c8c84402c
kayatk <thillaikayatk@gmail.com>
2024-07-31 17:46:12 +0200

    challenging DOM Script

--- herokuapp/src/main/java/Pages/ChallengingDOM.java
+++ herokuapp/src/main/java/Pages/ChallengingDOM.java
@@ -1,8 +1,10 @@
 package Pages;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
 
 public class ChallengingDOM {
     public static void main(String[] args) {
@@ -11,9 +13,9 @@ public class ChallengingDOM {
         driver.get("https://the-internet.herokuapp.com/challenging_dom");
         Assert.assertTrue(driver.getCurrentUrl().contains("challenging_dom"));
 
-        
-
-
+        //Locate the element using Relative elements/Friendly Locator
+       driver.findElement(RelativeLocator.with(By.tagName("td")).below(By.xpath("//tbody//td[1]")).above(By.xpath("//tbody//tr[3]")));
+        driver.quit();
     }
     
 }
