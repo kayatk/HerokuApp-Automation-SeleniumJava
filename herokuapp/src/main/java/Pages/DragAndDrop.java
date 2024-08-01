939aeb5e55a973b98deb7e961aee919f1211033d
kayatk <thillaikayatk@gmail.com>
2024-08-01 22:12:42 +0200

    Drag and Drop script

--- herokuapp/src/main/java/Pages/DragAndDrop.java
+++ herokuapp/src/main/java/Pages/DragAndDrop.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 
 import junit.framework.Assert;
 
@@ -13,8 +15,23 @@ public class DragAndDrop {
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/drag_and_drop");
         WebElement element = driver.findElement(By.tagName("h3"));
-       System.out.println(element.getText()); 
-       // Assert.assertEquals("", "");
+        Assert.assertEquals("element.getText()", "Drag and Drop");
+
+        WebElement source = driver.findElement(By.id("column-a"));
+        WebElement target =driver.findElement(By.id("column-b"));
+
+        //Action class
+        Actions action = new Actions(driver);
+        action.dragAndDrop(source, target);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        driver.quit();
     }
     
 }
