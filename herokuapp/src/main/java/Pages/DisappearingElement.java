2933980011c81d76a560e323b00d0ed56e714fdf
kayatk <thillaikayatk@gmail.com>
2024-08-03 00:06:00 +0200

    Disappearing Element Script

--- herokuapp/src/main/java/Pages/DisappearingElement.java
+++ herokuapp/src/main/java/Pages/DisappearingElement.java
@@ -3,12 +3,12 @@ package Pages;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
+
 
 public class DisappearingElement {
     public static void main(String[] args) {
@@ -17,25 +17,22 @@ public class DisappearingElement {
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/disappearing_elements");
 
-        WebElement gallery = driver.findElement(By.xpath("//a[text()='Gallery']"));
-
-       try {
-        gallery.isDisplayed();
-    } catch (Exception e) {
-        System.out.println("Element not found or not visible");
-
-    }
-    
-    driver.navigate().refresh();
-    
-        try {
-            gallery.isDisplayed();
+        
+        Boolean flag = false;
+        while(flag==false){
+            try {
+                WebElement gallery = driver.findElement(By.xpath("//a[text()='Gallery']"));
+                gallery.isDisplayed();
+                System.out.println("Element found or visible");
+             } catch (Exception e) {
+                System.out.println("Element not found or not visible");
+            }
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("window.location.reload(true)");
         }
-         catch (Exception e) {
-            System.out.println("Element not found or not visible");
-            
-        }
-
-        driver.quit();
+       
+        
+            driver.quit();
+    
     }
 }
