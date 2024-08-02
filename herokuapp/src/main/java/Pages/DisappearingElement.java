0bf65af7812ce76698141fd342294dd331752a0d
kayatk <thillaikayatk@gmail.com>
2024-08-02 23:53:01 +0200

    Disappearing Element Script

--- herokuapp/src/main/java/Pages/DisappearingElement.java
+++ herokuapp/src/main/java/Pages/DisappearingElement.java
@@ -17,9 +17,27 @@ public class DisappearingElement {
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/disappearing_elements");
 
-        WebElement gallery = driver.findElement(By.xpath("//a[text()='Gallery']"));
+        //WebElement gallery = driver.findElement(By.xpath("//a[text()='Gallery']"));
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
-        wait.until(ExpectedConditions.visibilityOf(gallery));
-        if(();
+       // wait.until(ExpectedConditions.visibilityOf(gallery));
+
+       try {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Gallery']")));
+        // Perform actions with the element, e.g., click or extract text
+        System.out.println("Element is visible: " + element.getText());
+    } catch (Exception e) {
+        System.out.println("Element not found or not visible");
+
+    }
+        driver.navigate().refresh();
+    
+        try {
+            WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Gallery']")));
+            // Perform actions with the element, e.g., click or extract text
+            System.out.println("Element is visible: " + element.getText());
+        } catch (Exception e) {
+            System.out.println("Element not found or not visible");
+            
+        }
     }
 }
