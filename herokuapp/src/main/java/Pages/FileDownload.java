7988c061dbd6f3a3a59802fc537976e3b0e476b9
kayatk <thillaikayatk@gmail.com>
2024-08-05 14:23:31 +0200

    File Download changes

--- herokuapp/src/main/java/Pages/FileDownload.java
+++ herokuapp/src/main/java/Pages/FileDownload.java
@@ -1,5 +1,6 @@
 package Pages;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,17 +12,35 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class FileDownload {
     public static void main(String[] args) {
-        WebDriver driver =new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://the-internet.herokuapp.com/download");
-        Assert.assertTrue(driver.getCurrentUrl().contains("download"));
         ChromeOptions option = new ChromeOptions();
         Map<String,Object> download =new HashMap<String,Object>();
         download.put("download.default_directory", "herokuapp/src/main/resources");
         option.setExperimentalOption("download", download);
-        driver.findElement(By.xpath("//a[text()='selenium-snapshot.png']")).click();
 
+        WebDriver driver =new ChromeDriver(option);
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/download");
+        Assert.assertTrue(driver.getCurrentUrl().contains("download"));
+    
+
+        driver.findElement(By.xpath("//a[text()='selenium-snapshot.png']")).click();
+        Assert.assertTrue(isFileAvailable(),"File Not Found");
         
     }
     
+    public boolean isFileAvailable(){
+
+        File folder =new File("herokuapp/src/main/resources");
+        File[] lFiles =folder.listFiles();
+        boolean isFileAvailable = false;
+        for(File x: lFiles){
+            if(x.isFile()){
+                String fileName =x.getName();
+                if ((fileName.matches(fileName))) {
+                    isFileAvailable =true;
+                }
+            }
+        }
+        return isFileAvailable;
+    }
 }
