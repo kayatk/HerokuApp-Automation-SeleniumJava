8dbf9cd0be97855b3aa5c4f2dbef2b77d6da6aa8
kayatk <thillaikayatk@gmail.com>
2024-08-05 14:14:46 +0200

    File Download changes

--- herokuapp/src/main/java/Pages/FileDownload.java
+++ herokuapp/src/main/java/Pages/FileDownload.java
@@ -1,8 +1,13 @@
 package Pages;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class FileDownload {
     public static void main(String[] args) {
@@ -10,6 +15,13 @@ public class FileDownload {
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/download");
         Assert.assertTrue(driver.getCurrentUrl().contains("download"));
+        ChromeOptions option = new ChromeOptions();
+        Map<String,Object> download =new HashMap<String,Object>();
+        download.put("download.default_directory", "herokuapp/src/main/resources");
+        option.setExperimentalOption("download", download);
+        driver.findElement(By.xpath("//a[text()='selenium-snapshot.png']")).click();
+
+        
     }
     
 }
