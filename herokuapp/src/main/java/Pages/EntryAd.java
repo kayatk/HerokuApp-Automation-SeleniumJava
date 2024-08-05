84e63afd6d4150a19f49cb21e87f21c9411da960
kayatk <thillaikayatk@gmail.com>
2024-08-05 11:03:17 +0200

    Entry Add changes

--- herokuapp/src/main/java/Pages/EntryAd.java
+++ herokuapp/src/main/java/Pages/EntryAd.java
@@ -17,8 +17,10 @@ public class EntryAd {
         try {
           // String windows = driver.getWindowHandle();    
            //System.out.println(driver.switchTo().window(windows).findElement(By.tagName("h3")).getText());
+           WebElement ele =driver.findElement(By.xpath("//div[@id = 'modal'] //h3 "));
+           System.out.println( ele.getText());
 
-          Assert.assertEquals( driver.findElement(By.xpath("//div[@id = 'modal'] //h3 ")).getText(), "This is a modal window"); 
+         // Assert.assertEquals( ele.getText(), "This is a modal window"); 
           driver.findElement(By.xpath("//p[text()='Close']")).click();
             
         }        
