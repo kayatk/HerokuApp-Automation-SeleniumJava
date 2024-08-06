2acc2a339091aab38e0af083a1645ba9e48ef8b3
kayatk <thillaikayatk@gmail.com>
2024-08-06 09:51:36 +0200

    File uploader changes

--- herokuapp/src/main/java/Pages/FileUploader.java
+++ herokuapp/src/main/java/Pages/FileUploader.java
@@ -1,5 +1,13 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FileUploader {
     
+    public static void main(String[] args) {
+        WebDriver driver =new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/upload");
+    }
 }
