38f5d024a80e7283ca01f137c461b5d97821e10c
kayatk <thillaikayatk@gmail.com>
2024-08-07 10:42:45 +0200

    Dynamic content changes

--- herokuapp/src/main/java/Pages/DynamicContent.java
+++ herokuapp/src/main/java/Pages/DynamicContent.java
@@ -14,20 +14,20 @@ public class DynamicContent {
         driver.get("https://the-internet.herokuapp.com/dynamic_content");
 
         Boolean flag= true;
-      //  while (flag==true){
-        try {
-            driver.findElement(By.xpath("//div[@id='content']/div[1]/div[2]")).getText().contains("Excepturi");
-           // System.out.println(driver.findElement(By.xpath("//div[@id='content']/div[1]/div[2]")).getText());
-            flag=false;
-            System.out.println("found");
-            //break;
-            
-        } catch (Exception e) {
-            driver.navigate().refresh();
-            //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-            Thread.sleep(1000);
+        while (flag==true){
+            try {
+                driver.findElement(By.xpath("//div[@id='content']//div[contains(text(),'Error')]"));
+                // System.out.println(driver.findElement(By.xpath("//div[@id='content']/div[1]/div[2]")).getText());
+                flag=false;
+                System.out.println("found");
+                //break;
+                
+                } catch (Exception e) {
+                    driver.navigate().refresh();
+                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+                //  Thread.sleep(1000);
+            }
         }
-   // }
-   driver.quit();
+        driver.quit();
     }
 }
