d1490c1b0b17d500fd4a9ab1441b30b19b661b07
kayatk <thillaikayatk@gmail.com>
2024-08-07 11:20:14 +0200

    Dyanamic Loading changes

--- herokuapp/src/main/java/Pages/DynamicLoading.java
+++ herokuapp/src/main/java/Pages/DynamicLoading.java
@@ -1,5 +1,13 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DynamicLoading {
+    public static void main(String[] args) {
+        WebDriver driver =new ChromeDriver();
+         driver.manage().window().maximize();
+         driver.get("https://the-internet.herokuapp.com/dynamic_loading");
+    }
     
 }
