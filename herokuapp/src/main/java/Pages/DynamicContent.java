223fc39fdea65a06caaec804f6bd3d2f04934a2b
kayatk <thillaikayatk@gmail.com>
2024-08-07 10:34:58 +0200

    Dynamic content changes

--- herokuapp/src/main/java/Pages/DynamicContent.java
+++ herokuapp/src/main/java/Pages/DynamicContent.java
@@ -1,25 +1,33 @@
 package Pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DynamicContent {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver =new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/dynamic_content");
 
         Boolean flag= true;
-        while (flag==true){
+      //  while (flag==true){
         try {
-            driver.findElement(By.xpath("//div[@id='content']/div[1]/div[2]/text()")).getText().contains("Recusandae");
+            driver.findElement(By.xpath("//div[@id='content']/div[1]/div[2]")).getText().contains("Excepturi");
+           // System.out.println(driver.findElement(By.xpath("//div[@id='content']/div[1]/div[2]")).getText());
+            flag=false;
+            System.out.println("found");
+            //break;
             
         } catch (Exception e) {
             driver.navigate().refresh();
-            flag = true;
+            //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+            Thread.sleep(1000);
         }
-    }
+   // }
+   driver.quit();
     }
 }
