5cc683c7245092dda4df979e625182690d9d862f
kayatk <thillaikayatk@gmail.com>
2024-08-09 11:07:46 +0200

    multiple windows script

--- herokuapp/src/main/java/Pages/MultipleWindows.java
+++ herokuapp/src/main/java/Pages/MultipleWindows.java
@@ -1,6 +1,9 @@
 package Pages;
 
+import java.util.Set;
+
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,6 +13,18 @@ public class MultipleWindows {
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/windows");
         Assert.assertTrue(driver.getCurrentUrl().contains("windows"));
+           
+       String parentWindow = driver.getWindowHandle();
+       driver.findElement(By.xpath("//a[text()='Click Here'] "));
+       Set<String> windows = driver.getWindowHandles();
+       for(String i :windows){
+            if(parentWindow != i){
+                driver.switchTo().window(i);
+                Assert.assertTrue(driver.getCurrentUrl().contains("new"));
+                Assert.assertTrue(driver.findElement(By.xpath("//h3[text()='New Window']")).isDisplayed());
+                driver.close();
+            }
+       }
 
         driver.quit();
     }
