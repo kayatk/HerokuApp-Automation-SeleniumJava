c0954eae4425a99331b485ea0ba85f4be98898f9
kayatk <thillaikayatk@gmail.com>
2024-08-09 11:47:05 +0200

    Mouse Hovers script

--- herokuapp/src/main/java/Pages/Hovers.java
+++ herokuapp/src/main/java/Pages/Hovers.java
@@ -1,7 +1,11 @@
 package Pages;
 
+import java.util.List;
+
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Hovers {
@@ -11,6 +15,11 @@ public class Hovers {
         driver.get("https://the-internet.herokuapp.com/hovers");
         Assert.assertTrue(driver.getCurrentUrl().contains("hovers"));
         
+        List<WebElement> elements = driver.findElements(By.xpath("//div[@class='figure']"));
+        for (WebElement webElement : elements) {
+            
+        }
+
         driver.quit();
     }
 }
