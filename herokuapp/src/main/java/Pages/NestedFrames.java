ceb09e29979a3357d3d0363cbe70b4431595cd98
kayatk <thillaikayatk@gmail.com>
2024-08-12 09:23:59 +0200

    iframes script

--- herokuapp/src/main/java/Pages/NestedFrames.java
+++ herokuapp/src/main/java/Pages/NestedFrames.java
@@ -1,7 +1,14 @@
 package Pages;
 
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class NestedFrames {
     public static void main(String[] args) {
-        
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/iframe");
+        Assert.assertTrue(driver.getCurrentUrl().contains("iframe"));
     }
 }
