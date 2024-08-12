e7f0ae07ff08b40721a9766b3b8f6d08709c9c3d
kayatk <thillaikayatk@gmail.com>
2024-08-12 09:37:44 +0200

    iframes script

--- herokuapp/src/main/java/Pages/NestedFrames.java
+++ herokuapp/src/main/java/Pages/NestedFrames.java
@@ -12,12 +12,15 @@ public class NestedFrames {
         driver.get("https://the-internet.herokuapp.com/iframe");
         Assert.assertTrue(driver.getCurrentUrl().contains("iframe"));
 
+        driver.findElement(By.xpath("//div[@class='tox-icon']")).click();
+
 
         driver.switchTo().frame("mce_0_ifr");
         Assert.assertTrue(driver.findElement(By.xpath("//p[contains(text(),'Your content')]")).isDisplayed());
         driver.switchTo().defaultContent();
        Boolean val= driver.findElement(By.xpath("//h3[contains(text(),'An iFrame')]")).isDisplayed();
        Assert.assertTrue(val);
+       driver.quit();
 
     }
 }
