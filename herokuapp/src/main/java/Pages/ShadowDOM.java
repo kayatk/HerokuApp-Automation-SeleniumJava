770d6aaeed49d3dc869f22d1d38034ef4883e095
kayatk <thillaikayatk@gmail.com>
2024-08-14 13:02:19 +0200

    Shadow DOM Script Changes

--- herokuapp/src/main/java/Pages/ShadowDOM.java
+++ herokuapp/src/main/java/Pages/ShadowDOM.java
@@ -1,5 +1,20 @@
 package Pages;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class ShadowDOM {
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/shadowdom");
+        Assert.assertTrue(driver.getCurrentUrl().contains("shadowdom"));
     
+    System.out.println(driver.findElement(By.xpath("//span")).getText());
+
+    driver.quit();
+    }
 }
