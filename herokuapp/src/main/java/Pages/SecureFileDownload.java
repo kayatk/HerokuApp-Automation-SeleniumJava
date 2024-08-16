de0b2609fc8fac94a8a2a6c5e493e363f0d1a24f
kayatk <thillaikayatk@gmail.com>
2024-08-16 20:59:00 +0200

    Secure File Download

--- herokuapp/src/main/java/Pages/SecureFileDownload.java
+++ herokuapp/src/main/java/Pages/SecureFileDownload.java
@@ -3,9 +3,14 @@ package Pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import junit.framework.Assert;
+
 public class SecureFileDownload {
+    public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://practice.expandtesting.com/shadowdom");
         Assert.assertTrue(driver.getCurrentUrl().contains("shadowdom"));
+    }
+        
 }
