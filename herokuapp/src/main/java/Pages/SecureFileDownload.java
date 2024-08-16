fed7d7a994cc5938287ca76365d963aa662a7ed5
kayatk <thillaikayatk@gmail.com>
2024-08-16 21:18:57 +0200

    Secure File Download

--- herokuapp/src/main/java/Pages/SecureFileDownload.java
+++ herokuapp/src/main/java/Pages/SecureFileDownload.java
@@ -9,10 +9,10 @@ public class SecureFileDownload {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://practice.expandtesting.com/shadowdom");
+        driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
         Assert.assertTrue(driver.getCurrentUrl().contains("shadowdom"));
 
-        admin
+        
     }
         
 }
