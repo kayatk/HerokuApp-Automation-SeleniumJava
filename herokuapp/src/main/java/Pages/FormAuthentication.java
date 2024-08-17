968bf1351d5039d424e97025026c11bdd1db6ed9
kayatk <thillaikayatk@gmail.com>
2024-08-17 11:21:27 +0200

    Form Authentication script

--- herokuapp/src/main/java/Pages/FormAuthentication.java
+++ herokuapp/src/main/java/Pages/FormAuthentication.java
@@ -1,5 +1,15 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Assert;
+
 public class FormAuthentication {
-    
+    public static void main(String[] args) {
+        WebDriver driver =new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/login");
+        Assert.assertTrue(driver.getCurrentUrl().contains("login"));
+    }
 }
