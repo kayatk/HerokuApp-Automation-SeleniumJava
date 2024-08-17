21871a67ec6952255aa6af9d2b7a0f0499712cfa
kayatk <thillaikayatk@gmail.com>
2024-08-17 22:12:55 +0200

    Json reader

--- herokuapp/src/main/java/Pages/FormAuthentication.java
+++ herokuapp/src/main/java/Pages/FormAuthentication.java
@@ -12,8 +12,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import com.google.gson.JsonObject;
-
 import junit.framework.Assert;
 
 public class FormAuthentication {
