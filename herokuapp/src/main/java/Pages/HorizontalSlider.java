80d42bf192470561e8f60641b14a497fa1271bdb
kayatk <thillaikayatk@gmail.com>
2024-08-18 11:46:39 +0200

    Horizontal slider

--- herokuapp/src/main/java/Pages/HorizontalSlider.java
+++ herokuapp/src/main/java/Pages/HorizontalSlider.java
@@ -1,7 +1,21 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Assert;
+
 public class HorizontalSlider {
     public static void main(String[] args) {
+        
+        WebDriver driver =new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/horizontal_slider");
+        Assert.assertTrue(driver.getCurrentUrl().contains("horizontal_slider"));
+
+        driver.findElement(By.xpath("//input[@type='range']"));
+
         
     }
     
