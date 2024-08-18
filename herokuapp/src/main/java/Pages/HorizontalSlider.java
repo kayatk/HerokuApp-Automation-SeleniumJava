aaf41d4dae398bd0fbb1027e5d527a0076cdac57
kayatk <thillaikayatk@gmail.com>
2024-08-18 12:03:34 +0200

    Horizontal slider

--- herokuapp/src/main/java/Pages/HorizontalSlider.java
+++ herokuapp/src/main/java/Pages/HorizontalSlider.java
@@ -1,8 +1,11 @@
 package Pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import junit.framework.Assert;
 
@@ -14,8 +17,14 @@ public class HorizontalSlider {
         driver.get("https://the-internet.herokuapp.com/horizontal_slider");
         Assert.assertTrue(driver.getCurrentUrl().contains("horizontal_slider"));
 
-        driver.findElement(By.xpath("//input[@type='range']"));
-
+        WebElement slider =driver.findElement(By.xpath("//input[@type='range']"));
+        //Using Send Keys
+            slider.click();
+            slider.sendKeys(Keys.ARROW_RIGHT);
+            slider.sendKeys(Keys.ARROW_LEFT);
+        //Using ACtion Class
+            new Actions(driver).dragAndDropBy(slider, 30, 0).perform();
+         driver.quit();
         
     }
     
